cf25e211f4b574a0ff332b866f82421a46680c4d
JaidenSP <jaidenpaterson3@gmail.com>
2022-06-21 16:12:55 +0930

    Backup for a current plan of the 16x16 grid of divs

--- assets/app.js
+++ assets/app.js
@@ -1,17 +1,39 @@
-//Using the dom to grab the container div in the html file
+
+
+ //Using the dom to grab the container div in the html file
  var container = document.getElementById('container-div');
 
-//create a column and row divs that span 16x16 using the dom
-var row = document.createElement('div');
-var column = document.createElement('div');
+ //using a for statement to create multiple divs
+ for (i = 0; i < 256; i++) {
 
-//adding the row class to the row element
-row.classList.add('row');
+   //create a column and row divs that span 16x16 using the dom
+   var row = document.createElement('div');
 
-//adding the column class to the column element
-column.classList.add('column');
+   //adding the row class to the row element
+   row.classList.add('row');
 
-//appending the new divs to the parent container
-container.appendChild(column);
-container.appendChild(row);
+   //appending the row child to the parent div
+   container.appendChild(row);
+
+    //styling the divs 
+  row.style.width = "100px";
+  row.style.height = "100px";
+  row.style.flexWrap;
+  row.style.flexDirection = "row";
+  row.style.display = "inline";
+  row.style.float = "left";
+  row.style.backgroundColor = "grey";
+ }
+
+
+ 
+
+
+
+
+
+
+
+
+
 
